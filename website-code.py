06b66a3abd330f3d8be84a3ad0494f78d67ba43c
Sys-stack <shivasudharsank2008@gmail.com>
2024-09-20 23:31:40 +0530

    Update website-code.py

--- website-code.py
+++ website-code.py
@@ -1,7 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
 
+#def image
+def scale_img(image_path,x_axis,y_axis):
+  img = Image.open(image_path)
+  resized_image = image.resize((x_axis,y_axis))
+  return resized_image
+  
+  
 #page configuration
 st.set_page_config(
   page_title = "DrAniList",
@@ -13,4 +21,7 @@ st.set_page_config(
            'font':'sans serif',
            'textColor':''
           })
-
+print(scale_img("https://github.com/Sys-stack/DrAniLIst/blob/image/HorizontalLine1.png?raw=true",300,40))
+#page header and title
+st.title("DrAniList")
+st.image(scale_img("https://github.com/Sys-stack/DrAniLIst/blob/image/HorizontalLine1.png?raw=true",300,40))
