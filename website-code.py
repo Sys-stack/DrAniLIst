a40f3cee93a99e9d28de0ad0512c4c60cc279565
Sys-stack <shivasudharsank2008@gmail.com>
2024-09-20 22:05:58 +0530

    Create website-code.py

---
+++ website-code.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+#page configuration
+st.set_page_config(
+  page_title = "DrAniList",
+  page_icon = "",
+  layout = 'wide',
+  initial_sidebar_state = 'collapsed',
+  theme = {'primaryColor':'',
+           'backgroundColor:'',
+           'font':'sans serif',
+           'textColor':''
+          })
+
